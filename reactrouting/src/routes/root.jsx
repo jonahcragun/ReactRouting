b7d250a6aa1def76046b4888a074ad80e291391d
Jonah Cragun <jonahcragun@gmail.com>
2024-09-06 19:54:43 -0600

    added responsive loading feature

--- reactrouting/src/routes/root.jsx
+++ reactrouting/src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link, useLoaderData, Form, redirect, NavLink } from "react-router-dom";
+import { Outlet, Link, useLoaderData, Form, redirect, NavLink, useNavigation } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function action() {
@@ -8,6 +8,8 @@ export async function action() {
 
 export default function Root() {
   const { contacts } = useLoaderData();
+  const navigation = useNavigation();
+
   return (
     <>
       <div id="sidebar">
@@ -69,7 +71,10 @@ export default function Root() {
           )}  
         </nav>
       </div>
-      <div id="detail">
+      <div id="detail"
+        className={
+            navigation.state === "loading" ? "loading" : ""
+        }>
         <Outlet />
       </div>
     </>
